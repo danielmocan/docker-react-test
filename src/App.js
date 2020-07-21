9e9583cd7329d96a0aae8bed347f7289bea9b5d0
Daniel Mocan <contact_daniel@yahoo.com>
2020-07-21 15:00:06 +0300

    Added text to feature branch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> & save to reload.
+          FEATURE BRANCH
         </p>
         <a
           className="App-link"
